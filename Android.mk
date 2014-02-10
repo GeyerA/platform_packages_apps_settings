81abb9c923f7d9f02a9efbcb6f2de95f6790df5f
GeyerA <calo.geyer@gmail.com>
2014-02-10 19:08:19 +0100

    Removed Performance Control

--- Android.mk
+++ Android.mk
@@ -8,31 +8,20 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := \
         $(call all-java-files-under, src) \
-        src/com/android/settings/EventLogTags.logtags \
-        $(call all-java-files-under, ../PerformanceControl/src) \
-        $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) \
-        $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
-
-LOCAL_RESOURCE_DIR := \
-        $(LOCAL_PATH)/res \
-        packages/apps/PerformanceControl/res \
-        $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res \
-        $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res
-
-LOCAL_ASSET_DIR += packages/apps/PerformanceControl/assets
-
-LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
-
-LOCAL_AAPT_FLAGS := --auto-add-overlay \
-        --extra-packages com.brewcrewfoo.performance \
-        --extra-packages com.koushikdutta.superuser \
-        --extra-packages com.koushikdutta.widgets
+        src/com/android/settings/EventLogTags.logtags
 
 LOCAL_PACKAGE_NAME := Settings
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS := --extra-packages com.koushikdutta.superuser:com.koushikdutta.widgets --auto-add-overlay
+
+LOCAL_SRC_FILES += $(call all-java-files-under,../../../external/koush/Superuser/Superuser/src) $(call all-java-files-under,../../../external/koush/Widgets/Widgets/src)
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../../../external/koush/Widgets/Widgets/res $(LOCAL_PATH)/../../../external/koush/Superuser/Superuser/res
 
 include $(BUILD_PACKAGE)
 
