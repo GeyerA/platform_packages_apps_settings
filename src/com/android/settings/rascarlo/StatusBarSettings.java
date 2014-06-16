ab0e6b3c1fef983ac905088abd606d8176380731
GeyerA <calo.geyer@gmail.com>
2014-06-16 12:38:47 +0200

    Added Network stats - Credit to Lichti

--- src/com/android/settings/rascarlo/StatusBarSettings.java
+++ src/com/android/settings/rascarlo/StatusBarSettings.java
@@ -1,11 +1,16 @@
 
 package com.android.settings.rascarlo;
 
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.res.Resources;
+import android.net.TrafficStats;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceGroup;
 import android.provider.Settings;
 
 import com.android.settings.R;
@@ -18,14 +23,32 @@ OnPreferenceChangeListener {
     // Quick Pulldown
     private static final String QUICK_PULLDOWN = "quick_pulldown";
 
+    // Network Traffic
+    private static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+    private static final String NETWORK_TRAFFIC_UNIT = "network_traffic_unit";
+    private static final String NETWORK_TRAFFIC_PERIOD = "network_traffic_period";
+
     // Quick Pulldown
     private ListPreference mQuickPulldown;
+
+    // Network Traffic
+    private ListPreference mNetTrafficState;
+    private ListPreference mNetTrafficUnit;
+    private ListPreference mNetTrafficPeriod;
+
+    private int mNetTrafficVal;
+    private int MASK_UP;
+    private int MASK_DOWN;
+    private int MASK_UNIT;
+    private int MASK_PERIOD;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.status_bar_settings);
+
+        loadResources();
 
         // Quick Settings pull down
         mQuickPulldown = (ListPreference) getPreferenceScreen().findPreference(QUICK_PULLDOWN);
@@ -35,6 +58,41 @@ OnPreferenceChangeListener {
                 Settings.System.QS_QUICK_PULLDOWN, 0);
         mQuickPulldown.setValue(String.valueOf(quickPulldownValue));
         updatePulldownSummary(quickPulldownValue);
+
+            // Network Traffic
+            mNetTrafficState = (ListPreference) getPreferenceScreen().findPreference(NETWORK_TRAFFIC_STATE);
+            mNetTrafficUnit = (ListPreference) getPreferenceScreen().findPreference(NETWORK_TRAFFIC_UNIT);
+            mNetTrafficPeriod = (ListPreference) getPreferenceScreen().findPreference(NETWORK_TRAFFIC_PERIOD);
+
+            // TrafficStats will return UNSUPPORTED if the device does not support it.
+            if (TrafficStats.getTotalTxBytes() != TrafficStats.UNSUPPORTED &&
+                    TrafficStats.getTotalRxBytes() != TrafficStats.UNSUPPORTED) {
+                mNetTrafficVal = Settings.System.getInt(getActivity().getContentResolver(), Settings.System.NETWORK_TRAFFIC_STATE, 0);
+                int intIndex = mNetTrafficVal & (MASK_UP + MASK_DOWN);
+                intIndex = mNetTrafficState.findIndexOfValue(String.valueOf(intIndex));
+                if (intIndex <= 0) {
+                    mNetTrafficUnit.setEnabled(false);
+                    mNetTrafficPeriod.setEnabled(false);
+                }
+                mNetTrafficState.setValueIndex(intIndex >= 0 ? intIndex : 0);
+                mNetTrafficState.setSummary(mNetTrafficState.getEntry());
+                mNetTrafficState.setOnPreferenceChangeListener(this);
+
+                mNetTrafficUnit.setValueIndex(getBit(mNetTrafficVal, MASK_UNIT) ? 1 : 0);
+                mNetTrafficUnit.setSummary(mNetTrafficUnit.getEntry());
+                mNetTrafficUnit.setOnPreferenceChangeListener(this);
+
+                intIndex = (mNetTrafficVal & MASK_PERIOD) >>> 16;
+                intIndex = mNetTrafficPeriod.findIndexOfValue(String.valueOf(intIndex));
+                mNetTrafficPeriod.setValueIndex(intIndex >= 0 ? intIndex : 1);
+                mNetTrafficPeriod.setSummary(mNetTrafficPeriod.getEntry());
+                mNetTrafficPeriod.setOnPreferenceChangeListener(this);
+            } else {
+                getPreferenceScreen().removePreference(findPreference(NETWORK_TRAFFIC_STATE));
+                getPreferenceScreen().removePreference(findPreference(NETWORK_TRAFFIC_UNIT));
+                getPreferenceScreen().removePreference(findPreference(NETWORK_TRAFFIC_PERIOD));
+            }
+
     }
 
     private void updatePulldownSummary(int value) {
@@ -55,8 +113,55 @@ OnPreferenceChangeListener {
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.QS_QUICK_PULLDOWN, quickPulldownValue);
             updatePulldownSummary(quickPulldownValue);
+
+        } else if (preference == mNetTrafficState) {
+            int intState = Integer.valueOf((String)objValue);
+            mNetTrafficVal = setBit(mNetTrafficVal, MASK_UP, getBit(intState, MASK_UP));
+            mNetTrafficVal = setBit(mNetTrafficVal, MASK_DOWN, getBit(intState, MASK_DOWN));
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NETWORK_TRAFFIC_STATE, mNetTrafficVal);
+            int index = mNetTrafficState.findIndexOfValue((String) objValue);
+            mNetTrafficState.setSummary(mNetTrafficState.getEntries()[index]);
+            if (intState == 0) {
+                mNetTrafficUnit.setEnabled(false);
+                mNetTrafficPeriod.setEnabled(false);
+            } else {
+                mNetTrafficUnit.setEnabled(true);
+                mNetTrafficPeriod.setEnabled(true);
+            }
+        } else if (preference == mNetTrafficUnit) {
+            // 1 = Display as Byte/s; default is bit/s
+            mNetTrafficVal = setBit(mNetTrafficVal, MASK_UNIT, ((String)objValue).equals("1"));
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NETWORK_TRAFFIC_STATE, mNetTrafficVal);
+            int index = mNetTrafficUnit.findIndexOfValue((String) objValue);
+            mNetTrafficUnit.setSummary(mNetTrafficUnit.getEntries()[index]);
+        } else if (preference == mNetTrafficPeriod) {
+            int intState = Integer.valueOf((String)objValue);
+            mNetTrafficVal = setBit(mNetTrafficVal, MASK_PERIOD, false) + (intState << 16);
+            Settings.System.putInt(getActivity().getContentResolver(), Settings.System.NETWORK_TRAFFIC_STATE, mNetTrafficVal);
+            int index = mNetTrafficPeriod.findIndexOfValue((String) objValue);
+            mNetTrafficPeriod.setSummary(mNetTrafficPeriod.getEntries()[index]);
             return true;
         }
         return false;
+    }
+
+    private void loadResources() {
+        Resources resources = getActivity().getResources();
+        MASK_UP = resources.getInteger(R.integer.maskUp);
+        MASK_DOWN = resources.getInteger(R.integer.maskDown);
+        MASK_UNIT = resources.getInteger(R.integer.maskUnit);
+        MASK_PERIOD = resources.getInteger(R.integer.maskPeriod);
+    }
+
+    // intMask should only have the desired bit(s) set
+    private int setBit(int intNumber, int intMask, boolean blnState) {
+        if (blnState) {
+            return (intNumber | intMask);
+        }
+        return (intNumber & ~intMask);
+    }
+
+    private boolean getBit(int intNumber, int intMask) {
+        return (intNumber & intMask) == intMask;
     }
 }
