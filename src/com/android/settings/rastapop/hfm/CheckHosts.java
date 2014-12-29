lichti1901/android_packages_apps_settings

Settings: Add Ad Blocker (2/3)

src/com/android/settings/terminus/hfm/CheckHosts.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.terminus.hfm;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class CheckHosts extends Service {
+
+    private static final String TAG = "HFMCheckHosts";
+
+    public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    private Context mContext;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        mContext = this;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Log.i(TAG, "Started check");
+        HfmHelpers.checkStatus(mContext);
+    }
+}

src/com/android/settings/terminus/hfm/FetchHosts.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.terminus.hfm;
+
+import java.io.IOException;
+
+import com.android.settings.terminus.hfm.HfmHelpers;
+
+public class FetchHosts {
+
+    public static String[] stringUrl = {
+        "http://hosts-file.net/.%5Cad_servers.txt",
+        "http://winhelp2002.mvps.org/hosts.txt",
+        "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
+        "http://adaway.org/hosts.txt"
+    };
+
+    public static int successfulSources;
+
+    public static void fetch() throws IOException {
+
+        String cmd = "mount -o rw,remount /system && echo \'foo\' > /etc/started.cfg && sed -i -e '$G' /etc/hosts.alt";
+
+        successfulSources = 0;
+
+        int i = 0;
+        while (i <= 3) {
+            if (HfmHelpers.isAvailable(stringUrl[i])) {
+                cmd = cmd + " && wget \"" + stringUrl[i] + "\" -O /etc/hosts" + i; //Get file
+                cmd = cmd + " && sed -i -e '$G' /etc/hosts" + i; //Put newline
+                successfulSources++;
+            }
+            i++;
+        }
+
+        cmd = cmd + " && cat /etc/hosts[0-9] /etc/hosts.alt > /etc/hosts.tmp" //Merge old & new hosts
+                  + " && sort -u /etc/hosts.tmp -o /etc/hosts.tmp" //Remove duplicate lines
+                  + " && sed -i '/^[@#]/ d' /etc/hosts.tmp" //Remove commented lines
+                  + " && sed -i '/^$/d' /etc/hosts.tmp" //Remove blank lines
+                  + " && cp -f /etc/hosts.tmp /etc/hosts.alt"
+                  + " && rm -f /etc/hosts[0-9] /etc/hosts.tmp /etc/started.cfg" //Clean up
+                  + " && mount -o ro,remount /system";
+
+       HfmHelpers.RunAsRoot(cmd);
+    }
+}

src/com/android/settings/terminus/hfm/HfmSettings.java
@@ -1,0 +1,113 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.terminus.hfm;
+
+import java.io.IOException;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.View;
+
+import com.android.settings.terminus.hfm.FetchHosts;
+import com.android.settings.terminus.hfm.HfmHelpers;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class HfmSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "HfmSettings";
+    private static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    public static ProgressDialog pd;
+
+    ConnectivityManager connMgr;
+
+    public static SwitchPreference mHfmDisableAds;
+    Preference mHfmUpdateHosts;
+
+    Context context;
+
+    public static Resources res;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = getActivity();
+        res = getResources();
+        pd = new ProgressDialog(context);
+        pd.setCancelable(false);
+        pd.setMessage(res.getString(R.string.hfm_dialog_wait));
+        connMgr = (ConnectivityManager) getSystemService(context.CONNECTIVITY_SERVICE);
+
+        addPreferencesFromResource(R.xml.terminus_hfm_settings);
+        ContentResolver resolver = context.getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mHfmDisableAds = (SwitchPreference) findPreference(HFM_DISABLE_ADS);
+        mHfmDisableAds.setChecked((Settings.System.getInt(resolver,
+            Settings.System.HFM_DISABLE_ADS, 0) == 1));
+        mHfmDisableAds.setOnPreferenceChangeListener(this);
+        mHfmUpdateHosts = prefScreen.findPreference("hfm_update_hosts");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        boolean value = (Boolean) objValue;
+        if (preference == mHfmDisableAds) {
+            Settings.System.putInt(resolver,
+                    Settings.System.HFM_DISABLE_ADS, value ? 1 : 0);
+            HfmHelpers.checkStatus(getActivity());
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHfmUpdateHosts) {
+            try {
+                HfmHelpers.checkConnectivity(context, connMgr);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        return true;
+    }
+}

src/com/android/settings/terminus/hfm/IntentReceiver.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.terminus.hfm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class IntentReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "HFMBootReceiver";
+
+    public static final String ACTION_RUN_BOOTCOMPLETE = "android.intent.action.BOOT_COMPLETED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Started Receiver");
+        Intent serv = new Intent(context, CheckHosts.class);
+        serv.setAction(intent.getAction());
+        serv.putExtras(intent);
+        context.startService(serv);
+    }
+}

GeyerA/platform_packages_apps_settings

Conflicts:
	res/values/custom_strings.xml
	res/xml/terminus_settings.xml

src/com/android/settings/rastapop/hfm/CheckHosts.java
@@ -1,0 +1,49 @@
+/*
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.rastapop.hfm;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class CheckHosts extends Service {
+
+    private static final String TAG = "HFMCheckHosts";
+
+    public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    private Context mContext;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        mContext = this;
+    }
+
+    @Override
+    public void onStart(Intent intent, int startId) {
+        Log.i(TAG, "Started check");
+        HfmHelpers.checkStatus(mContext);
+    }
+}

src/com/android/settings/rastapop/hfm/FetchHosts.java
@@ -1,0 +1,61 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.rastapop.hfm;
+
+import java.io.IOException;
+
+import com.android.settings.rastapop.hfm.HfmHelpers;
+
+public class FetchHosts {
+
+    public static String[] stringUrl = {
+        "http://hosts-file.net/.%5Cad_servers.txt",
+        "http://winhelp2002.mvps.org/hosts.txt",
+        "http://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext",
+        "http://adaway.org/hosts.txt"
+    };
+
+    public static int successfulSources;
+
+    public static void fetch() throws IOException {
+
+        String cmd = "mount -o rw,remount /system && echo \'foo\' > /etc/started.cfg && sed -i -e '$G' /etc/hosts.alt";
+
+        successfulSources = 0;
+
+        int i = 0;
+        while (i <= 3) {
+            if (HfmHelpers.isAvailable(stringUrl[i])) {
+                cmd = cmd + " && wget \"" + stringUrl[i] + "\" -O /etc/hosts" + i; //Get file
+                cmd = cmd + " && sed -i -e '$G' /etc/hosts" + i; //Put newline
+                successfulSources++;
+            }
+            i++;
+        }
+
+        cmd = cmd + " && cat /etc/hosts[0-9] /etc/hosts.alt > /etc/hosts.tmp" //Merge old & new hosts
+                  + " && sort -u /etc/hosts.tmp -o /etc/hosts.tmp" //Remove duplicate lines
+                  + " && sed -i '/^[@#]/ d' /etc/hosts.tmp" //Remove commented lines
+                  + " && sed -i '/^$/d' /etc/hosts.tmp" //Remove blank lines
+                  + " && cp -f /etc/hosts.tmp /etc/hosts.alt"
+                  + " && rm -f /etc/hosts[0-9] /etc/hosts.tmp /etc/started.cfg" //Clean up
+                  + " && mount -o ro,remount /system";
+
+       HfmHelpers.RunAsRoot(cmd);
+    }
+}

src/com/android/settings/rastapop/hfm/HfmSettings.java
@@ -1,0 +1,113 @@
+/*
+ * Copyright (C) 2014 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.rastapop.hfm;
+
+import java.io.IOException;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.View;
+
+import com.android.settings.rastapop.hfm.FetchHosts;
+import com.android.settings.rastapop.hfm.HfmHelpers;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class HfmSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "HfmSettings";
+    private static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+    public static ProgressDialog pd;
+
+    ConnectivityManager connMgr;
+
+    public static SwitchPreference mHfmDisableAds;
+    Preference mHfmUpdateHosts;
+
+    Context context;
+
+    public static Resources res;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = getActivity();
+        res = getResources();
+        pd = new ProgressDialog(context);
+        pd.setCancelable(false);
+        pd.setMessage(res.getString(R.string.hfm_dialog_wait));
+        connMgr = (ConnectivityManager) getSystemService(context.CONNECTIVITY_SERVICE);
+
+        addPreferencesFromResource(R.xml.hfm_settings);
+        ContentResolver resolver = context.getContentResolver();
+        PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mHfmDisableAds = (SwitchPreference) findPreference(HFM_DISABLE_ADS);
+        mHfmDisableAds.setChecked((Settings.System.getInt(resolver,
+            Settings.System.HFM_DISABLE_ADS, 0) == 1));
+        mHfmDisableAds.setOnPreferenceChangeListener(this);
+        mHfmUpdateHosts = prefScreen.findPreference("hfm_update_hosts");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        boolean value = (Boolean) objValue;
+        if (preference == mHfmDisableAds) {
+            Settings.System.putInt(resolver,
+                    Settings.System.HFM_DISABLE_ADS, value ? 1 : 0);
+            HfmHelpers.checkStatus(getActivity());
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mHfmUpdateHosts) {
+            try {
+                HfmHelpers.checkConnectivity(context, connMgr);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        return true;
+    }
+}

src/com/android/settings/rastapop/hfm/IntentReceiver.java
@@ -1,0 +1,39 @@
+/*
+ * Copyright (C) 2014 Dirty Unicorns
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.rastapop.hfm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class IntentReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "HFMBootReceiver";
+
+    public static final String ACTION_RUN_BOOTCOMPLETE = "android.intent.action.BOOT_COMPLETED";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.i(TAG, "Started Receiver");
+        Intent serv = new Intent(context, CheckHosts.class);
+        serv.setAction(intent.getAction());
+        serv.putExtras(intent);
+        context.startService(serv);
+    }
+}